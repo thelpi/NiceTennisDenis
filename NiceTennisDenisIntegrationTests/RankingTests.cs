816c927eb70534b24385f7a6e5df2efbe7b89c2c
Lpi <thelpi.v3@gmail.com>
2019-06-16 00:20:00 +0200

    Conversion des tests pour interroger la WebApi.

--- NiceTennisDenisIntegrationTests/RankingTests.cs
+++ NiceTennisDenisIntegrationTests/RankingTests.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NiceTennisDenisIntegrationTests.Properties;
 
 namespace NiceTennisDenisIntegrationTests
 {
     [TestClass]
     public class RankingTests
     {
+        private Tuple<uint, uint> Request(uint versionId, DateTime date, uint playerId)
+        {
+            var request = (HttpWebRequest)WebRequest.Create($"http://localhost:52368/api/Ranking/atp/debug/{versionId}/{date.ToString("yyyy-MM-dd")}/{playerId}");
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    using (var streamReader = new System.IO.StreamReader(responseStream))
+                    {
+                        return Newtonsoft.Json.JsonConvert.DeserializeObject<Tuple<uint, uint>>(streamReader.ReadToEnd());
+                    }
+                }
+            }
+        }
+
         [TestMethod]
         public void DebugRankingForPlayer_DelPotro_2018_December()
         {
-            Initialize();
-            var result = NiceTennisDenisDll.DataController.Default.DebugRankingForPlayer(105223, 2, new DateTime(2018, 12, 24));
+            var result = Request(2, new DateTime(2018, 12, 24), 105223);
+
             Assert.IsNotNull(result);
             Assert.AreEqual((uint)5300, result.Item1);
             Assert.AreEqual((uint)15, result.Item2);
@@ -20,22 +35,11 @@ namespace NiceTennisDenisIntegrationTests
         [TestMethod]
         public void DebugRankingForPlayer_Dimitrov_2017_December()
         {
-            Initialize();
-            var result = NiceTennisDenisDll.DataController.Default.DebugRankingForPlayer(105777, 2, new DateTime(2017, 12, 25));
+            var result = Request(2, new DateTime(2017, 12, 25), 105777);
+
             Assert.IsNotNull(result);
             Assert.AreEqual((uint)5150, result.Item1);
             Assert.AreEqual((uint)23, result.Item2);
-        }
-
-        private void Initialize()
-        {
-            NiceTennisDenisDll.DataController.InitializeDefault(
-                string.Format(Settings.Default.sqlConnStringPattern,
-                    Settings.Default.sqlServer,
-                    Settings.Default.sqlDatabase,
-                    Settings.Default.sqlUser,
-                    Settings.Default.sqlPassword
-                ), Settings.Default.datasDirectory, Settings.Default.isWta, Settings.Default.configurationId).LoadModel();
         }
     }
 }
